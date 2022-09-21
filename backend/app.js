aac6ff409303a2ccd5a43a8b3c584dab93938847
Erin Clark <erincodepirate@gmail.com>
2022-09-20 17:16:50 -0700

    Add basic express to backend

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+  res.send('hello api');
+})
+
+app.listen(3000, 
+()=>{
+    console.log('server running http://localhost:3000');
+})
