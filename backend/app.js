7a126e2f360505cda72a57bfd18d7172075bc47c
Erin Clark <erincodepirate@gmail.com>
2022-09-20 17:36:14 -0700

    Add dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+require('dotenv/config');
 
-app.get('/', (req, res)=>{
+const api = process.env.API_URL;
+
+app.get(api+'/', (req, res)=>{
   res.send('hello api');
 })
 
