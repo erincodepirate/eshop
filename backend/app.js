a790f3fdefd01310a149ffd5ba4443e6a598bcb8
Erin Clark <erincodepirate@gmail.com>
2022-09-21 17:24:47 -0700

    Add morgan logger

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 require('dotenv/config');
+const morgan = require('morgan');
 
 const api = process.env.API_URL;
 
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.post(`${api}/products`, (req, res)=>{
   const newProduct = req.body;
