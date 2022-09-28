2de6be54e6836a1e4bceb07f34f94580c71a7619
Erin Clark <erincodepirate@gmail.com>
2022-09-27 17:40:37 -0700

    Add get/post database methods

--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,37 @@ const api = process.env.API_URL;
 app.use(express.json());
 app.use(morgan('tiny'));
 
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  countInStock: {type: Number, required: true}
+});
+
+const Product = mongoose.model('Product', productSchema);
+
 app.post(`${api}/products`, (req, res)=>{
-  const newProduct = req.body;
-  res.send(newProduct);
-})
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock
+  });
+
+  product.save().then(createdProduct => {
+    res.status(201).json(createdProduct);
+  }).catch(err => {
+    res.status(500).json({error: err, success: false});
+  })
+});
+
+app.get(`${api}/products`, async (req, res)=>{
+  const productList = await Product.find();
+
+  if(!productList) {
+    res.status(500).json({success: false})
+  }
+
+  res.send(productList);
+});
 
 mongoose.connect('mongodb://127.0.0.1:27017/eshop').then(()=> {
   console.log('Database Connection is ready...')
