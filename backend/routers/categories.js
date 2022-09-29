2fb2cc2b2a9c2d94989d55ee4d6a43488dfad6b8
Erin Clark <erincodepirate@gmail.com>
2022-09-28 17:50:00 -0700

    Add create and delete category methods

--- backend/routers/categories.js
+++ backend/routers/categories.js
@@ -2,21 +2,36 @@ const express = require('express');
 const router = express.Router();
 const {Category} = require('../models/category');
 
-router.post(`/`, (req, res)=>{
-  const category = new Category({
+router.post(`/`, async (req, res)=>{
+  let category = new Category({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color
   });
 
-  category.save().then(createdCategory => {
-    res.status(201).json(createdCategory);
-  }).catch(err => {
-    res.status(500).json({error: err, success: false});
-  })
+  category = await category.save();
+  if (!category) {
+    return res.status(404).send('The category cannot be created!')
+  }
+  return res.send(category);
+});
+
+router.delete(`/:id`, (req, res)=>{
+  Category.findByIdAndRemove(req.params.id).then(category => {
+    if (category) {
+      return res.status(200).json({success:true, message:'The category is deleted.'})
+    } else {
+      return res.status(404).json({success:false, message:'Category not found.'});
+    }
+  }).catch(err=>{
+    return res.status(400).json({success:false, error: err});
+  });
 });
 
 router.get(`/`, async (req, res)=>{
   const categoryList = await Category.find();
 
-  if(!categoryList) {
+  if (!categoryList) {
     res.status(500).json({success: false})
   }
 
