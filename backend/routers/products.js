0e211eab732db285722190d3ce90f2a207ee9dad
Erin Clark <erincodepirate@gmail.com>
2022-10-10 17:56:39 -0700

    Add get method for individual product details, limit details on get

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -31,13 +31,23 @@ router.post(`/`, async (req, res)=>{
 });
 
 router.get(`/`, async (req, res)=>{
-  const productList = await Product.find();
+  const productList = await Product.find().select('name image -_id');
 
   if(!productList) {
     res.status(500).json({success: false})
   }
 
   res.send(productList);
+});
+
+router.get(`/:id`, async (req, res)=>{
+  const product = await Product.findById(req.params.id);
+
+  if(!product) {
+    res.status(500).json({success: false})
+  }
+
+  res.send(product);
 });
 
 module.exports = router;
