fc22cf8112b527f1b33b75968bd2f683199daf87
Erin Clark <erincodepirate@gmail.com>
2022-10-17 17:51:49 -0700

    Add user creation method

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -2,15 +2,27 @@ const express = require('express');
 const router = express.Router();
 const {User} = require('../models/user');
 
-router.post(`/`, (req, res)=>{
-  const user = new User({
+router.post(`/`, async (req, res)=>{
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    color: req.body.color,
+    passwordHash: req.body.passwordHash,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country
   });
 
-  user.save().then(createdUser => {
-    res.status(201).json(createdUser);
-  }).catch(err => {
-    res.status(500).json({error: err, success: false});
-  })
+  user = await user.save();
+
+  if(!user) {
+    res.status(400).json('the user could not be created or registered')
+  }
+
+  res.send(user)
 });
 
 router.get(`/`, async (req, res)=>{
