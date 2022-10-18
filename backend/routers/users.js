b355a227da56eab22ad2ead51b889e83e9f8dc95
Erin Clark <erincodepirate@gmail.com>
2022-10-17 18:13:42 -0700

    Add user update method

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const {User} = require('../models/user');
 const bcrypt = require('bcryptjs');
 
+const salt = 10;
+
 router.post(`/`, async (req, res)=>{
   let user = new User({
     name: req.body.name,
     email: req.body.email,
     color: req.body.color,
-    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    passwordHash: bcrypt.hashSync(req.body.password, salt),
     phone: req.body.phone,
     isAdmin: req.body.isAdmin,
     apartment: req.body.apartment,
@@ -43,6 +45,37 @@ router.get(`/:id`, async (req, res)=>{
   }
 
   res.status(200).send(user);
+});
+
+router.put(`/:id`, async (req, res)=>{
+  const userExist = await User.findById(req.params.id);
+  if (!userExist) {
+    res.status(500).json({success: false, message: 'The user with the given id was not found'})
+  }
+  let newPassword = userExist.passwordHash;
+  if (req.body.password) { // if new password
+    newPassword = bcrypt.hashSync(req.body.password, salt);
+  }
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      color: req.body.color,
+      passwordHash: newPassword,
+      phone: req.body.phone,
+      isAdmin: req.body.isAdmin,
+      apartment: req.body.apartment,
+      zip: req.body.zip,
+      city: req.body.city,
+      country: req.body.country
+    },
+    { new: true } // return the new updated data
+  );
+  if (!user) {
+    return res.status(400).send('The user cannot be updated!')
+  }
+  return res.send(user);
 });
 
 module.exports = router;
