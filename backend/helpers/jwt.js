79e7b351db475918893adb5d791697a380499f01
Erin Clark <erincodepirate@gmail.com>
2022-10-18 18:14:25 -0700

    Add exclusions to jwt authentication

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -2,7 +2,18 @@ const { expressjwt: jwt } = require("express-jwt");
 
 function expressJwt() {
     const secret = process.env.secret;
-    return jwt({secret: secret, algorithms: ["HS256"]})
+    const api = process.env.API_URL;
+    return jwt({
+        secret: secret, 
+        algorithms: ["HS256"]
+    }).unless({
+        path: [
+            {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
+            {url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
+            `${api}/users/login`,
+            `${api}/v1/users/register`
+        ]
+    })
 }
 
 module.exports = expressJwt;
