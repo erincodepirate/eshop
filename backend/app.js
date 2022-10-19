245f807f0ed27dbdabc7ac3be7defa0d0754f415
Erin Clark <erincodepirate@gmail.com>
2022-10-18 17:49:42 -0700

    Add express-jwt

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const { expressjwt: jwt } = require("express-jwt");
 
 require('dotenv/config');
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.options('*', cors());
 
 const api = process.env.API_URL;
+const secret = process.env.secret;
 
 // routes
 const categoriesRouter = require('./routers/categories');
@@ -19,6 +21,7 @@ const usersRouter = require('./routers/users');
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(jwt({secret: secret, algorithms: ["HS256"]}));
 
 app.use(`${api}/categories`, categoriesRouter);
 app.use(`${api}/orders`, ordersRouter);
