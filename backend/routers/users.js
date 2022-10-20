084339836a47c822e5f16a45abfb529b25068ca3
Erin Clark <erincodepirate@gmail.com>
2022-10-19 17:44:28 -0700

    Add additional user methods

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -30,6 +30,29 @@ router.post(`/`, async (req, res)=>{
   res.send(user)
 });
 
+router.post(`/register`, async (req, res)=>{
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    color: req.body.color,
+    passwordHash: bcrypt.hashSync(req.body.password, salt),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country
+  });
+
+  user = await user.save();
+
+  if(!user) {
+    res.status(400).json('the user could not be created or registered')
+  }
+
+  res.send(user)
+});
+
 router.get(`/`, async (req, res)=>{
   const userList = await User.find().select('-passwordHash');
 
@@ -94,6 +117,28 @@ router.post('/login', async(req,res) => {
     return res.status(200).send({user: user.email, token: token});
   }
   return res.status(400).send('password is wrong');
-})
+});
+
+router.get(`/get/count`, async (req, res)=> {
+  const userCount = await User.countDocuments();
+
+  if(!userCount){
+    res.status(500).json({success: false})
+  }
+  res.send({userCount: userCount})
+
+});
+
+router.delete(`/:id`, (req, res)=>{
+  User.findByIdAndRemove(req.params.id).then(user => {
+    if (user) {
+      return res.status(200).json({success:true, message:'The user is deleted.'})
+    } else {
+      return res.status(404).json({success:false, message:'User not found.'});
+    }
+  }).catch(err=>{
+    return res.status(400).json({success:false, error: err});
+  });
+});
 
 module.exports = router;
