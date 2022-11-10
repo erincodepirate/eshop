82151af012227470a45df092b526c3d1b33f5fdc
Erin Clark <erincodepirate@gmail.com>
2022-11-09 17:56:19 -0800

    Generate UI components

--- raccoonshop/apps/raccoonshop/src/app/app.module.ts
+++ raccoonshop/apps/raccoonshop/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { UiModule } from '@raccoonshop/ui';
 
 const routes: Routes = [{
     path: '',
@@ -20,7 +21,7 @@ const routes: Routes = [{
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
