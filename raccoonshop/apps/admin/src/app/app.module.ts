dec5a88361c615a07da3feabdb5ea7f3625ef82f
Erin Clark <erincodepirate@gmail.com>
2022-11-16 17:51:26 -0800

    Add dashboard component

--- raccoonshop/apps/admin/src/app/app.module.ts
+++ raccoonshop/apps/admin/src/app/app.module.ts
@@ -3,15 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+const routes: Routes = [{
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, ShellComponent, SidebarComponent],
+  declarations: [AppComponent, NxWelcomeComponent, ShellComponent, SidebarComponent, DashboardComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
