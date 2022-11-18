29b391b7edd8ff0caa25e64ff2aafd6a588ba42b
Erin Clark <erincodepirate@gmail.com>
2022-11-17 17:51:22 -0800

    Add categories list page

--- raccoonshop/apps/admin/src/app/app.module.ts
+++ raccoonshop/apps/admin/src/app/app.module.ts
@@ -7,6 +7,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+const UX_MODULE = [CardModule, ToolbarModule, ButtonModule, TableModule];
 
 const routes: Routes = [{
     path: '',
@@ -15,16 +23,21 @@ const routes: Routes = [{
       {
         path: 'dashboard',
         component: DashboardComponent
+      },
+      {
+        path: 'categories',
+        component: CategoriesListComponent
       }
     ]
   },
 ];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, ShellComponent, SidebarComponent, DashboardComponent],
+  declarations: [AppComponent, NxWelcomeComponent, ShellComponent, SidebarComponent, DashboardComponent, CategoriesListComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+    [...UX_MODULE]
   ],
   providers: [],
   bootstrap: [AppComponent],
